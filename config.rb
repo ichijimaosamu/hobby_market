8763d97379cdf661c29f649538eb9ac7539a0ead
ichijimaosamu <ichijimaosamu@gmail.com>
2021-11-11 18:52:19 +0900

    テンプレートファイル追加

---
+++ config.rb
@@ -0,0 +1,12 @@
+﻿Encoding.default_external = 'utf-8'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+
+sourcemap = true
+line_comments = false
+
+output_style = :expanded
