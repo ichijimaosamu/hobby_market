87d0614761317468047eccedbb2df8f5074e4ac8
ichijimaosamu <ichijimaosamu@gmail.com>
2022-09-22 18:45:42 +0900

    調整

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,10 @@
-var events = [ // ※※※※ 設定月は0からカウント 1月→0 2月→1 3月→2 
-  {'Date': new Date(2022, 0, 20), 'Title': '〇〇イベント開催', 'Link': 'https://hobbymarket.ents.jp/'},
-  {'Date': new Date(2022, 0, 18), 'Title': '●●イベント開催', 'Link': 'https://hobbymarket.ents.jp/'},
-  {'Date': new Date(2022, 0, 27), 'Title': '〇〇イベント開催', 'Link': 'https://hobbymarket.ents.jp/'},
+var events = [ // ※※※※ 設定月は0からカウント 1月→0 2月→1 3月→2
+  {'Date': new Date(2022, 8, 20), 'Title': '〇〇イベント開催', 'Link': 'https://stg.hobima.com/test_osamu/hobby_market/'},
+  {'Date': new Date(2022, 8, 18), 'Title': '●●イベント開催', 'Link': 'https://stg.hobima.com/test_osamu/hobby_market/'},
+  {'Date': new Date(2022, 8, 27), 'Title': '〇〇イベント開催', 'Link': 'https://stg.hobima.com/test_osamu/hobby_market/'},
+  {'Date': new Date(2022, 9, 20), 'Title': '〇〇イベント開催', 'Link': 'https://stg.hobima.com/test_osamu/hobby_market/'},
+  {'Date': new Date(2022, 9, 18), 'Title': '●●イベント開催', 'Link': 'https://stg.hobima.com/test_osamu/hobby_market/'},
+  {'Date': new Date(2022, 9, 27), 'Title': '〇〇イベント開催', 'Link': 'https://stg.hobima.com/test_osamu/hobby_market/'},
 ];
 
 
